3552900cbea5a99a01c72d4c1651404a6cdcd5bc
Lisa Ross <lisa@avenuego.com>
2022-01-01 18:52:09 -0500

    add image

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import Image from 'next/image';
 
-import Head from 'next/head'
+const YourComponent = () => (
+  <Image
+    src="/images/gravatar.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Lisa Ross"
+  />
+);
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -210,5 +219,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
